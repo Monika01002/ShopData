4c2385508b95dd5561059cdc61377b9ead003b84
Monika01002 <141919611+Monika01002@users.noreply.github.com>
2026-08-23 12:16:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ headers_D = {
 url_D = os.environ.get('URL_D')
 res = s.get(url_D, headers=headers_D, verify=False)
 data = res.json()
+flattened_data = []
+
+for item in data:
+    if 'mstShop' in item and isinstance(item['mstShop'], dict):
+        flattened_data.append(item['mstShop'])
+    else:
+        flattened_data.append(item)
 
 with open("Shopdate.json", 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    json.dump(flattened_data, f, ensure_ascii=False, indent=2)
